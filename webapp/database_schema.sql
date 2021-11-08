482cb1e3bceca8f876c2141897e3ef026c71a9d2
elliothanson <hansone2@carleton.edu>
2021-11-08 14:52:42 -0600

    database schema

---
+++ webapp/database_schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE parks{
+park_code text,
+park_name text,
+state_id integer,
+acreage text,
+longitude text,
+latitude text
+};
+
+CREATE TABLE states{
+id serial,
+state_code text
+};
+
+CREATE TABLE species{
+id serial,
+park_code text,
+category_id integer,
+order_id integer,
+family_id integer,
+scientific_name text,
+common_names text,
+nativeness text
+};
+
+CREATE TABLE categories{
+id serial,
+category text
+};
+
+CREATE TABLE orders{
+id serial
+order text
+};
+
+CREATE TABLE families{
+id serial,
+family text
+};
